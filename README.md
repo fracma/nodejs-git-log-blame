723406268210fad0193f974178204503b46d4683
jvanhouteghem <vanhouteghem.j@gmail.com>
2019-05-19 00:17:13 +0200

    feat(gitlog & gitblame): init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 nodejs-git-log-blame
+
+Sandbox with a lot of feat and fix to test some mix with git log and git blame
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/* const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
@@ -7,4 +7,63 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+}) */
+
+//gitBlameExample();
+gitBlame()
+
+function gitBlameExample() {
+  const gitlog = require('gitlog');
+
+  const options =
+  {
+    repo: __dirname + '/'
+    , number: 20
+    , author: 'jvanhouteghem'
+    , fields:
+      ['hash'
+        , 'abbrevHash'
+        , 'subject'
+        , 'authorName'
+        , 'authorDateRel'
+      ]
+    , execOptions:
+    {
+      maxBuffer: 1000 * 1024
+    }
+  };
+
+  // Asynchronous (with Callback)
+  gitlog(options, function (error, commits) {
+    // Commits is an array of commits in the repo
+    console.log(commits)
+  });
+
+  // Synchronous
+  let commits = gitlog(options);
+  console.log(commits);
+}
+
+function gitBlameExample2() {
+  var gitBlame = require('git-blame');
+  var path = require('path');
+
+  var repoPath = path.resolve(process.env.REPO || (__dirname + '/.git'));
+  var file = process.env.FILE || 'package.json';
+  var rev = process.env.REV || 'HEAD';
+
+  gitBlame(repoPath, {
+    file: file,
+    rev: rev
+  }).on('data', function (type, data) {
+    // type can be 'line' or 'commit'
+    console.log(type, data);
+  }).on('error', function (err) {
+    console.error(err.message);
+    process.exit(1);
+  }).on('end', function () {
+    console.log('±±±±±±±±±±±±±±±±±±');
+    console.log("That's all, folks!");
+  });
+}
+
